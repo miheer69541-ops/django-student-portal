2a1502b2cb381823be127cff0448bd54fe2bb005
BlacK <miheer69541@gmail.com>
2026-03-11 21:38:10 +0530

    Added update and delete functionality for students

--- students/views.py
+++ students/views.py
@@ -1,7 +1,7 @@
-
-from django.shortcuts import render, redirect
-from .forms import StudentForm
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Student
+from .forms import StudentForm
+
 
 def home(request):
     students = Student.objects.all()
@@ -17,4 +17,23 @@ def register(request):
         form = StudentForm()
 
     return render(request,'register.html',{'form':form})
-    
+
+
+def update_student(request, id):
+    student = get_object_or_404(Student, id=id)
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = StudentForm(instance=student)
+
+    return render(request,'register.html',{'form':form})
+
+
+def delete_student(request, id):
+    student = get_object_or_404(Student, id=id)
+    student.delete()
+    return redirect('home')
